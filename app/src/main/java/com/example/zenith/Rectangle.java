94caf61597bc8b67df7104cbff57fd52bc0cd7f9
vanshtaneja23 <155813476+vanshtaneja23@users.noreply.github.com>
2025-09-26 14:29:52 -0600

    Update Rectangle.java

--- app/src/main/java/com/example/zenith/Rectangle.java
+++ app/src/main/java/com/example/zenith/Rectangle.java
@@ -1,6 +1,13 @@
 package com.example.zenith;
 
 public class Rectangle extends Shape{
+    int width;
+    int height;
+    public Rectangle(int x, int y, int w, int h) {
+        this.x = x; this.y = y;
+        this.width = w; this.height = h;
+    }
+
     
 
 }
